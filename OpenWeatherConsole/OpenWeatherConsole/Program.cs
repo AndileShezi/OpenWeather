273b8e51418aa0696b7d048c461a3bf87dbded92
AndileShezi <ag.shezi@gmail.com>
2019-10-03 16:58:01 +0200

    Commit
    Weatherr Shandis

---
+++ OpenWeatherConsole/OpenWeatherConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using OpenWeatherLibrary;
+using System;
+using System.Threading.Tasks;
+
+namespace OpenWeatherConsole
+{
+    class Program
+    {
+    static async Task Main(string[] args)
+        {
+            WeatherGenerator weather = new WeatherGenerator();
+            string weatherNow = await weather.GetWeatherAsync();
+            Console.WriteLine(weatherNow);
+        }
+    }
+}
+
---
+++ OpenWeatherConsole/OpenWeatherLibrary/WeatherGenerator.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OpenWeatherLibrary
+{
+    public class WeatherGenerator
+    {
+        public static async Task<string> GetWeatherAsync()
+
+        {
+            HttpClient httpClient = new HttpClient();
+
+            string cpt = await httpClient.GetStringAsync("http://api.openweathermap.org/data/2.5/weather?q=Cape%20Town&appid=f56fd6fd6dd6b3c61a68b9224700830d");
+
+           LocalWeather Localweather = JsonConvert.DeserializeObject<LocalWeather>(cpt);
+
+            var returnString = "";
+            
+
+
+            foreach (Weather weather in LocalWeather.weather)
+            {
+                returnString = weather.description;
+             }
+
+            return cpt;
+
+        }
+}
+}
